1737c83ef58172a86b957674f31a3634db8d9e48
hzk <1>
2023-04-27 00:39:43 +0800

    机考自测04-27-01

--- src/main/java/com/jikao/enterprise/hw/doublepoint/HwDoublePointTest1.java
+++ src/main/java/com/jikao/enterprise/hw/doublepoint/HwDoublePointTest1.java
@@ -1,24 +1,21 @@
 package com.jikao.enterprise.hw.doublepoint;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * 6.双指针（3题）
  * (1) *leetcode 674.最长连续递增序列
  * (2) NC5.最长回文子串
- * (3) NC28.最小覆盖子串7.深搜（1题）
- * (1) HJ41.称砝码
+ * (3) NC76.最小覆盖子串
  */
 public class HwDoublePointTest1 {
 
-    private List<String> list2 = new ArrayList<>();
 
     public static void main(String[] args) throws Exception {
         HwDoublePointTest1 my = new HwDoublePointTest1();
 //        test1();
-        my.test2();
-//        test3();
+//        my.test2();
+        my.test3();
 //        test4();
     }
 
@@ -63,48 +60,129 @@ public class HwDoublePointTest1 {
         String str1 = "babad";
         String result1 = longestPalindrome(str1);
         System.out.println(result1);
-        list2.clear();
+
         System.out.println("----------");
         String str2 = "cbbd";
         String result2 = longestPalindrome(str2);
         System.out.println(result2);
     }
 
-    public String longestPalindrome(String str) {
-        if (str.length() < 2) {
-            return str;
+    /**
+     * 方法二：中心扩展算法
+     * @param s
+     * @return
+     */
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) {
+            return "";
         }
-        char[] charArray = str.toCharArray();
-        for (int i = 0; i < charArray.length; i++) {
-            int index = charArray.length;
-            while (index > i+1) {
-                String tempString = str.substring(i, index);
-                isValid(tempString);
-                index--;
+        int maxLen = 1;
+        int begin = 0;
+
+        char[] charArray = s.toCharArray();
+        for (int i = 0; i < s.length()-1; i++) {
+            int oddLen = expandAroundCenter(charArray, i, i);
+            int evenLen = expandAroundCenter(charArray, i, i+1);
+
+            int curMaxLen = Math.max(oddLen, evenLen);
+            if (curMaxLen > maxLen) {
+                maxLen = curMaxLen;
+                begin = i - (maxLen - 1)/2;
             }
         }
-        if (list2.size()>0) {
-            return list2.get(0);
-        }
-        return null;
+        return s.substring(begin, begin + maxLen);
     }
 
-    private boolean isValid(String str) {
-        boolean flag = true;
-        char[] charArray = str.toCharArray();
-        int length = charArray.length;
-        int index = 0;
-        for (int i = length-1; i >= 0; i--) {
-            if (charArray[i] != charArray[index]) {
-                flag = false;
+    /**
+     *
+     * @param charArray
+     * @param left
+     * @param right
+     * @return 回文串长度
+     */
+    public int expandAroundCenter(char[] charArray, int left, int right) {
+        // left = right，回文中心是一个字符，回文串长度为奇数
+        // left = right+1，回文中心是两个字符，回文串长度为偶数
+        int len = charArray.length;
+        int i = left;
+        int j = right;
+        while (i >=0 && j < len) {
+            if (charArray[i] == charArray[j]) {
+                i--;
+                j++;
+            } else {
                 break;
             }
-            index++;
         }
-        if (flag) {
-            list2.add(str);
-        }
-        return flag;
+
+        // j-i+1-2=j-i-1
+        return j-i-1;
     }
+
+
+    Map<Character, Integer> ori = new HashMap<Character, Integer>();
+    Map<Character, Integer> cnt = new HashMap<Character, Integer>();
+
+    /**
+     * (3) NC76.最小覆盖子串
+     * 给你一个字符串 s 、一个字符串 t 。返回 s 中涵盖 t 所有字符的最小子串。如果 s 中不存在涵盖 t 所有字符的子串，则返回空字符串 "" 。
+     *
+     * 输入：s = "ADOBECODEBANC", t = "ABC"
+     * 输出："BANC"
+     * 解释：最小覆盖子串 "BANC" 包含来自字符串 t 的 'A'、'B' 和 'C'。
+     *
+     * @throws Exception
+     */
+    private void test3() throws Exception{
+        String s = "ADOBECODEBANC";
+        String t = "ABC";
+        String s1 = minWindow(s, t);
+        System.out.println(s1);
+    }
+
+    public String minWindow(String s, String t) {
+        int tLen = t.length();
+        for (int i = 0; i < tLen; i++) {
+            char c = t.charAt(i);
+            ori.put(c, ori.getOrDefault(c, 0) + 1);
+        }
+        int l = 0, r = -1;
+        int len = Integer.MAX_VALUE, ansL = -1, ansR = -1;
+        int sLen = s.length();
+        while (r < sLen) {
+            ++r;
+            if (r < sLen && ori.containsKey(s.charAt(r))) {
+                cnt.put(s.charAt(r), cnt.getOrDefault(s.charAt(r), 0) + 1);
+            }
+            while (check() && l <= r) {
+                if (r - l + 1 < len) {
+                    len = r - l + 1;
+                    ansL = l;
+                    ansR = l + len;
+                }
+                if (ori.containsKey(s.charAt(l))) {
+                    cnt.put(s.charAt(l), cnt.getOrDefault(s.charAt(l), 0) - 1);
+                }
+                ++l;
+            }
+        }
+        return ansL == -1 ? "" : s.substring(ansL, ansR);
+    }
+
+    public boolean check() {
+        Iterator iter = ori.entrySet().iterator();
+        while (iter.hasNext()) {
+            Map.Entry entry = (Map.Entry) iter.next();
+            Character key = (Character) entry.getKey();
+            Integer val = (Integer) entry.getValue();
+            if (cnt.getOrDefault(key, 0) < val) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+
 
 }
---
+++ src/main/java/com/jikao/enterprise/hw/other/HwOtherTest1.java
@@ -0,0 +1,23 @@
+package com.jikao.enterprise.hw.other;
+
+/**
+ * 9.其他（6题）
+ * (1) *HJ108.求最小公倍数
+ * (2) *HJ28.素数伴侣
+ * (3) *HJ60.查找组成一个偶数最接近的两个素数
+ * (4) *leetcode 994.腐烂的橘子
+ * (5) leetcode 204.计数质数
+ * (6) HJ25. 数据分类处理
+ */
+public class HwOtherTest1 {
+
+    public static void main(String[] args) throws Exception {
+//        test1();
+//        test2();
+//        test3();
+//        test4();
+//        test5();
+//        test6();
+    }
+
+}
